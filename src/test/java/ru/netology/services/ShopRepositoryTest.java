38a0cf4c4e65a816d15da0c101ff8a2077cede55
Brovkin Ivan <isbrovkinn@gmail.com>
2023-05-17 11:09:49 +0300

    Initial commit

---
+++ src/test/java/ru/netology/services/ShopRepositoryTest.java
@@ -0,0 +1,41 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopRepositoryTest {
+    @Test
+    public void testRemoveWhenProductExist() {
+        ShopRepository shopRepository = new ShopRepository();
+        Product product1 = new Product(1, "Телефон",15000);
+        Product product2 = new Product(2, "Ноутбук",45000);
+        Product product3 = new Product(3, "Книга",5000);
+
+        shopRepository.add(product1);
+        shopRepository.add(product2);
+        shopRepository.add(product3);
+        shopRepository.remove(2);
+        Product[] actual = shopRepository.findAll();
+        Product[] expected = {product1, product3};
+
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void testRemoveWhenProductNotExist() {
+        ShopRepository shopRepository = new ShopRepository();
+        Product product1 = new Product(1, "Телефон",15000);
+        Product product2 = new Product(2, "Ноутбук",45000);
+        Product product3 = new Product(3, "Книга",5000);
+
+        shopRepository.add(product1);
+        shopRepository.add(product2);
+        shopRepository.add(product3);
+        Assertions.assertThrows(NotFoundException.class,
+                ()-> shopRepository.remove(4)
+        );
+    }
+
+
+}
